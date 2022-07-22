b3aa07917883a17a5bd559d2ef16c296be71288a
akanshaaa19 <asakhre2002@gmail.com>
2022-07-22 14:34:17 +0530

    new-project

--- src/projects.js
+++ src/projects.js
@@ -17,9 +17,9 @@ const projects = [
     },
     {
         index: 3,
-        github: "https://github.com/akanshaaa19/todo-app",
-        active: "",
-        title: "To-Do App",
+        github: "https://github.com/akanshaaa19/tasks",
+        active: "https://warm-escarpment-16353.herokuapp.com/",
+        title: "Tasks App",
         description: "In this app you can add todos, cross them off or delete them later on. You can also create diferent lists for different activities.",
         stack: "NodeJS EJS MongoDB"
     },
