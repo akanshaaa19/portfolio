ed352e422f12096a0ec57bc4ce8f2327cc050d57
akanshaaa19 <asakhre2002@gmail.com>
2022-11-23 22:47:15 +0530

    configured templates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,30 +1,35 @@
 import React from "react";
-import '../styles/About.css'
-import image from '../assests/me.jpg';
-import resume from '../assests/akansha_resume.pdf';
+import "../styles/About.css";
+import image from "../assests/me.jpg";
+import resume from "../assests/akansha_resume.pdf";
 
-function About(){
-    return(
-        <section id="about">
+function About() {
+  return (
+    <section id="about">
+      <div className="about">
+        <h2 className="sec-heading">/about me</h2>
+        <div className="row">
+          <div className="col-6 about-text">
+            <p>
+              I'm a third-year computer science student deeply interested in
+              full-stack development. I enjoy learning new things and taking on
+              new challenges. Along with all this, I enjoy attending hackathons,
+              networking with new people, and sharing ideas.
+            </p>
+            <a href={resume} download="akanshaResume">
+              <button className="download-btn btn btn-outline-light">
+                Resume <i className="fas fa-download"></i>
+              </button>
+            </a>
+          </div>
 
-        <div className="about">
-            <h2 className="sec-heading">/about me</h2>
-            <div className="row">
-            <div className="col-6 about-text">
-                <p>
-                I am currently a third-year <b>Computer Science</b> student who has a great interest in <b>full-stack development</b> as well as data science. I am someone who loves to take up new challenges and learn new skills. Along with all this, I like participating in Hackathons, meeting new people, and exchanging different ideas.                </p>
-
-            <a href={resume} download="akanshaResume"><button className="download-btn btn btn-outline-light">Resume <i className="fas fa-download"></i></button></a>
-
-            </div>
-
-            <div className="col-6 about-image">
-                <img className="self-picture" src={image} alt = "its me"/>
-            </div>
-            </div>
-            </div>
+          <div className="col-6 about-image">
+            <img className="self-picture" src={image} alt="its me" />
+          </div>
+        </div>
+      </div>
     </section>
-    );
+  );
 }
 
 export default About;
--- src/projects.js
+++ src/projects.js
@@ -1,36 +1,40 @@
 const projects = [
-    {
-        index: 1,
-        github: "https://github.com/akanshaaa19/weather-app",
-        active: "https://akanshaaa19.github.io/weather-app",
-        title: "Weather App",
-        description: "This takes a city name as an input, and returns the current temperature, the minimum and maximum temperature, pressure, humidity etc.",
-        stack: "ReactJS Bootstrap API"
-    },
-    {
-        index: 2,
-        github: "https://github.com/akanshaaa19/portfolio",
-        active: "https://akanshaaa19.github.io/portfolio",
-        title: "Portfolio",
-        description: "A  Portfolio application built on react, which describes me, my skills, and my projects",
-        stack: "ReactJS HTML Bootstrap"
-    },
-    {
-        index: 3,
-        github: "https://github.com/akanshaaa19/tasks",
-        active: "https://akanshaaa19.github.io/tasks",
-        title: "Tasks App",
-        description: "In this app you can add todos, cross them off or delete them later on. You can also create diferent lists for different activities.",
-        stack: "ReactJS DataBase ContextAPI"
-    },
-    // {
-    //     index: 4,
-    //     github: "https://github.com/akanshaaa19/notes-app",
-    //     active: "",
-    //     title: "Notes App",
-    //     description: "In this app you can write notes and save them as well as retrieve them later on. This stores all the notes on a database.",
-    //     stack: "NodeJS EJS MongoDB"
-    // }
-    ]
+  {
+    index: 1,
+    github: "https://github.com/akanshaaa19/weather-app",
+    active: "https://akanshaaa19.github.io/weather-app",
+    title: "Weather App",
+    description:
+      "This takes a city name as an input, and returns the current temperature, the minimum and maximum temperature, pressure, humidity etc.",
+    stack: "ReactJS Bootstrap API",
+  },
+  {
+    index: 2,
+    github: "https://github.com/akanshaaa19/portfolio",
+    active: "https://akanshaaa19.github.io/portfolio",
+    title: "Portfolio",
+    description:
+      "A  Portfolio application built on react, which describes me, my skills, and my projects",
+    stack: "ReactJS HTML Bootstrap",
+  },
+  {
+    index: 3,
+    github: "https://github.com/akanshaaa19/tasks",
+    active: "https://akanshaaa19.github.io/tasks",
+    title: "Tasks App",
+    description:
+      "In this app you can add todos, cross them off or delete them later on. You can also create diferent lists for different activities.",
+    stack: "ReactJS DataBase ContextAPI",
+  },
+  {
+    index: 4,
+    github: "https://github.com/akanshaaa19/resume-generator",
+    active: "https://akanshaaa19.github.io/resume-generator",
+    title: "Resume Generator",
+    description:
+      "This takes up your details and generates a resume, which you can download in PDF format and share.",
+    stack: "ReactJS Firebase ContextAPI",
+  },
+];
 
 export default projects;
